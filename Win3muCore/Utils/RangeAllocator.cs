e1b91c41e0f7241a3a4330fb773fa4806e076d0c
Brad Robinson <contact@toptensoftware.com>
2017-12-18 13:21:45 +1100

    License

--- Win3muCore/Utils/RangeAllocator.cs
+++ Win3muCore/Utils/RangeAllocator.cs
@@ -1,4 +1,22 @@
-﻿#define NO_CHECKED
+﻿/*
+win3mu - Windows 3 Emulator
+Copyright (C) 2017 Topten Software.
+
+win3mu is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+win3mu is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Stripes.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#define NO_CHECKED
 
 using System;
 using System.Collections.Generic;
